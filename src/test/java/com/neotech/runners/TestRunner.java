8ab83d67338f480bbb720a6e7bbd3372be354e26
DonmezFatih <91677563+DonmezFatih@users.noreply.github.com>
2026-05-19 23:18:38 -0400

    Screenshot Functionality test #2

--- src/test/java/com/neotech/runners/TestRunner.java
+++ src/test/java/com/neotech/runners/TestRunner.java
@@ -14,7 +14,7 @@ import io.cucumber.junit.CucumberOptions;
 
 		monochrome = true,
 
-		tags = "@Sprint-2@",
+		tags = "@Sprint-2",
 
 		plugin = { "pretty", "json:target/cucumber.json", "html:target/cucumber-report.html" })
 
